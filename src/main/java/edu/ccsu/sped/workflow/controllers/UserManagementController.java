a265fe00b34b314197801a031131ce3b1031d187
csmith-afk <csmith645@live.com>
2019-10-31 01:48:34 -0400

    Added UserService to UserManagement Controller

--- src/main/java/edu/ccsu/sped/workflow/controllers/UserManagementController.java
+++ src/main/java/edu/ccsu/sped/workflow/controllers/UserManagementController.java
@@ -1,11 +1,17 @@
 package edu.ccsu.sped.workflow.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.ccsu.sped.workflow.services.UserService;
+
 @Controller
 public class UserManagementController {
+	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/user-management")
 	public String userManagement() {
