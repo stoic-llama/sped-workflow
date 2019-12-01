bab431ff61200c0a6b1b77dfe8b3f73fd9ddd8b4
stoic-llama <stoic.llama@gmail.com>
2019-12-01 01:13:10 -0500

    Tied Workflow to Authenticated User On Home Page

--- src/main/java/edu/ccsu/sped/workflow/controllers/WorkflowManagementController.java
+++ src/main/java/edu/ccsu/sped/workflow/controllers/WorkflowManagementController.java
@@ -1,11 +1,13 @@
 package edu.ccsu.sped.workflow.controllers;
 
-import java.security.Principal;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,16 +28,27 @@ public class WorkflowManagementController {
 	
 	@Autowired
 	private UserService userService;
-    
-	@GetMapping("/")
+    	
+	@GetMapping(value={"/","/index"})
 	public String index(Model model) {
+		List<WorkflowHelper> workflowHelpers = new LinkedList<WorkflowHelper>();
+		workflowHelpers = getActiveWorkflowsByAuthenticatedUser();
+		Workflow workflow = new Workflow();	
+		model.addAttribute("activeWorkflows", workflowHelpers);
+		model.addAttribute("users", userService.getUsers());
+		model.addAttribute("workflow",workflow);	
+		return "index";
+	}
+	
+	@GetMapping(value={"/view-all-workflows"})
+	public String viewAllWorkflows(Model model) {
 		List<WorkflowHelper> workflowHelpers = new LinkedList<WorkflowHelper>();
 		workflowHelpers = getActiveWorkflowsWithUsers();
 		Workflow workflow = new Workflow();	
 		model.addAttribute("activeWorkflows", workflowHelpers);
 		model.addAttribute("users", userService.getUsers());
 		model.addAttribute("workflow",workflow);	
-		return "index";
+		return "view-all-workflows";
 	}
 	
 	@GetMapping("/workflow-management")
@@ -48,6 +61,34 @@ public class WorkflowManagementController {
 		model.addAttribute("workflow",workflow);	
 		return "workflow-management";
 	}
+	
+	@GetMapping(value = "/oneworkflow")
+	@ResponseBody
+	public Optional<Workflow> getWorkflowById(Integer Id, Model model) {
+		return workflowService.getWorkflowById(Id); 
+	}
+	
+	@PostMapping(value = "/addWorkflow/")
+	public String addWorkflow( Workflow workflow ) {
+		List <User> u = workflow.getUser();
+		
+		Optional<User> pr = userService.getUserById(workflow.getPrimaryReaderUid());
+		User primaryReader = pr.get();
+		u.add(primaryReader);
+		
+		Optional<User> s = userService.getUserById(workflow.getStudentUid());
+		User student = s.get();
+		u.add(student);
+		
+		Optional<User> sr = userService.getUserById(workflow.getSecondaryReaderUid());
+		User secondaryReader = sr.get();
+		u.add(secondaryReader);
+		
+		workflowService.addWorkflow(workflow);
+		
+		return "redirect:/workflow-management"; // go back to workflow-management page after updating database
+	}
+	
 	
 	
 	private List<WorkflowHelper> getActiveWorkflowsWithUsers () {
@@ -95,15 +136,36 @@ public class WorkflowManagementController {
 	}
 	
 	private List<WorkflowHelper> getActiveWorkflowsByAuthenticatedUser() {
-		// get all the workflows that are not equal to "DONE"
 		List<Workflow> workflows = workflowService.getWorkflows();
+		List<Workflow> activeWorkflows = new LinkedList<Workflow>();
 		List<WorkflowHelper> workflowHelpers = new LinkedList<WorkflowHelper>();
-		Principal principal;
+		List<Workflow> activeWorkflowsToUser = new LinkedList<Workflow>();
 		Optional<User> u;
 		User user = new User();
+		Boolean userAuthenticatedWithActiveWorkflow = false;
+
+		String username = getAuthenticatedUserName();
 		
+		// build list of all active workflows
 		for (Workflow workflow : workflows) {
-			if (!(workflow.getStatus().equals("DONE"))) { // Not equal Done
+			if (!(workflow.getStatus().equals("DONE"))) { // Not equal Done		
+				activeWorkflows.add(workflow);
+			}
+		}
+		
+		// trim the active workflow list to only workflows tied to authenticated user
+		for (Workflow workflow : activeWorkflows) {
+			if (getEmailFromWorkflow(workflow, "primaryReader").equals(username) || // authenticated user equal primary reader
+			    getEmailFromWorkflow(workflow, "secondaryReader").equals(username) || // or authenticated user equal secondary reader
+			    getEmailFromWorkflow(workflow, "student").equals(username)) { // or authenticated user equal to student
+					activeWorkflowsToUser.add(workflow);
+					userAuthenticatedWithActiveWorkflow = true;
+			}
+		}
+		
+		// build list of active workflows tied to authenticated user
+		if (userAuthenticatedWithActiveWorkflow == true) {
+			for (Workflow workflow : activeWorkflowsToUser) { 
 				WorkflowHelper w = new WorkflowHelper(); 
 				
 				// Get Wid
@@ -135,36 +197,45 @@ public class WorkflowManagementController {
 			}
 		}
 		
-		// return list of all active workflows with names, embodied in list of workflowHelpers
+		// return list of all active workflows tied to authenticated user
 		return workflowHelpers;
 	}
 	
-	
-	@GetMapping(value = "/oneworkflow")
-	@ResponseBody
-	public Optional<Workflow> getWorkflowById(Integer Id, Model model) {
-		return workflowService.getWorkflowById(Id); 
-	}
-	
-	@PostMapping(value = "/addWorkflow/")
-	public String addWorkflow( Workflow workflow ) {
-		List <User> u = workflow.getUser();
+	private String getEmailFromWorkflow(Workflow workflow, String userType) {
+		String email = "";
 		
 		Optional<User> pr = userService.getUserById(workflow.getPrimaryReaderUid());
 		User primaryReader = pr.get();
-		u.add(primaryReader);
 		
 		Optional<User> s = userService.getUserById(workflow.getStudentUid());
 		User student = s.get();
-		u.add(student);
 		
 		Optional<User> sr = userService.getUserById(workflow.getSecondaryReaderUid());
 		User secondaryReader = sr.get();
-		u.add(secondaryReader);
 		
-		workflowService.addWorkflow(workflow);
-		
-		return "redirect:/workflow-management"; // go back to workflow-management page after updating database
+		switch (userType) {
+			case "primaryReader":
+				email = primaryReader.getEmail();
+				break;
+			case "student":
+				email = student.getEmail();
+				break;
+			case "secondaryReader":
+				email = secondaryReader.getEmail();
+				break;
+		} // end switch
+		return email;
+	} // end getEmailFromWorkflow
+	
+	private String getAuthenticatedUserName() {
+		String username = "";
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = context.getAuthentication();
+		if (authentication != null && authentication.isAuthenticated()) {
+			username = authentication.getName();
+		}
+		return username;
 	}
+	
 
 }
